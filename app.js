6662a7b1b50a9b60777f41e78b853b0416603dba
George Gu <george.guzhangbo@gmail.com>
2015-05-06 20:41:59 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
+ */
+Ext.application({
+    name: 'NHC_LINK',
+
+    extend: 'NHC_LINK.Application',
+    
+    autoCreateViewport: 'NHC_LINK.view.main.Main'
+	
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to NHC_LINK.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
+});
---
+++ resources/Readme.md
@@ -0,0 +1,4 @@
+# NHC_LINK/resources
+
+This folder contains resources (such as images) needed by the application. This file can
+be removed if not needed.
